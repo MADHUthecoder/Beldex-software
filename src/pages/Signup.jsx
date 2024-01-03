9b188f8cff0a9b1a750d0b1c3e48cf840febafc6
MADHUthecoder <2k22csbs26@kiot.ac.in>
2024-01-03 20:33:32 +0530

    first commit

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import image from '../images/ecosystem.jpg'
+function signup() {
+  return (
+    <div>
+        <img src={image} alt='beldex ecosystem' className='h-[500px] w-[500px]'/>
+    </div>
+  )
+}
+
+export default signup
